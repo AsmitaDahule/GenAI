568e2bc6918ba436f3fa0b428b9f57f44c6152d9
Asmita Dahule <dahuleasmita@gmail.com>
2025-06-04 10:09:41 +0530

    llm with history maintain

---
+++ LLM2.js
@@ -0,0 +1,21 @@
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync';
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyBHiFLrBUFgtiMj6N0PVHq2Ce4mD4B-QpA" });
+
+const chat = ai.chats.create({
+    model: "gemini-2.0-flash",
+    history:[],
+})
+
+
+async function main() {
+    const userProblem = readlineSync.question("Ask me anything-->")
+    const response1 = await chat.sendMessage({
+        message: userProblem,
+      });
+
+      console.log(response1.text)
+    main();
+}
+await main();
